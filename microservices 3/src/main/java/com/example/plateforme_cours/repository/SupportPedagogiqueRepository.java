d6f8a7606917f2e6df83b3daf51b72e98f202e89
AbdelmoulaMoukrime <mkrabde@gmail.com>
2025-12-10 11:55:47 +0100

    SupportPedag

---
+++ microservices 3/src/main/java/com/example/plateforme_cours/repository/SupportPedagogiqueRepository.java
@@ -0,0 +1,9 @@
+package com.example.plateforme_cours.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.plateforme_cours.model.SupportPedagogique;
+
+public interface SupportPedagogiqueRepository extends JpaRepository<SupportPedagogique, Long> {
+
+}
