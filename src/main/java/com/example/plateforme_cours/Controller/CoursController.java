75e47183935a97a29d4266d40f43905cb9e846c7
AbdelmoulaMoukrime <mkrabde@gmail.com>
2025-12-10 10:58:51 +0100

    creat class coursController

---
+++ src/main/java/com/example/plateforme_cours/Controller/CoursController.java
@@ -0,0 +1,19 @@
+package com.example.plateforme_cours.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.plateforme_cours.model.Cours;
+import com.example.plateforme_cours.model.SupportPedagogique;
+import com.example.plateforme_cours.service.CoursService;
+
+@RestController
+@RequestMapping("/cours")
