97a821de84d9db9cdfba0d08dbe2eb78f40102aa
AbdelmoulaMoukrime <mkrabde@gmail.com>
2025-12-10 11:54:27 +0100

    LangageDeProgrammation

---
+++ microservices 3/src/main/java/com/example/plateforme_cours/repository/LangageDeProgrammationRepository.java
@@ -0,0 +1,9 @@
+package com.example.plateforme_cours.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.plateforme_cours.model.LangageDeProgrammation;
+
+public interface LangageDeProgrammationRepository extends JpaRepository<LangageDeProgrammation, Long> {
+
+}
